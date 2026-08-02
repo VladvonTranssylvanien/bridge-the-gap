1734695d70bde03dd14f1990278866f4f13b8e34
VladVonTranssilvanien <105380245+vladvontranssilvanien@users.noreply.github.com>
2026-08-02 10:19:11 +0200

    Add AWS OIDC federation for GitHub Actions, scoped to ECR push on service-a repo only

---
+++ terraform/aws/github-oidc.tf
@@ -0,0 +1,76 @@
+resource "aws_iam_openid_connect_provider" "github_actions" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["1c58a3a8518e8759bf075b76b750d4f2df264fcd"]
+
+  tags = {
+    Project   = "bridge-the-gap"
+    ManagedBy = "terraform"
+  }
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github_actions.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:VladvonTranssylvanien/bridge-the-gap:ref:refs/heads/main"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions_ecr_push" {
+  name               = "bridge-the-gap-github-actions-ecr-push"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+
+  tags = {
+    Project   = "bridge-the-gap"
+    ManagedBy = "terraform"
+  }
+}
+
+data "aws_iam_policy_document" "github_actions_ecr_push" {
+  statement {
+    sid       = "ECRAuth"
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ECRPush"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload",
+      "ecr:PutImage",
+    ]
+    resources = [aws_ecr_repository.service_a.arn]
+  }
+}
+
+resource "aws_iam_role_policy" "github_actions_ecr_push" {
+  name   = "ecr-push"
+  role   = aws_iam_role.github_actions_ecr_push.id
+  policy = data.aws_iam_policy_document.github_actions_ecr_push.json
+}
+
+output "github_actions_role_arn" {
+  value = aws_iam_role.github_actions_ecr_push.arn
+}
